c5cf94020b0b657f9ffe01ec9469b3b89a5957bc
⁨ﮩ٨ـнυѕĸy_Dєvﮩ٨ـﮩ⁩ <alfredojoseamaya4580@gmail.com>
2026-09-12 00:09:22 -0500

    Update API URL for Spotify integration

--- src/api/Download/Spotify.js
+++ src/api/Download/Spotify.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 module.exports = function (app) {
   const CREATOR = 'Husky API';
   const AUTHOR = 'ﮩ٨ـнυѕĸy_Dєvﮩ٨ـﮩ';
-  const API_URL = 'https://api.delirius.store';
+  const API_URL = 'https://api.delirius.online';
 
   function isSpotifyUrl(text = '') {
     return /^(https?:\/\/)?(www\.)?(open\.spotify\.com|spotify\.link)\/.+$/i.test(text);
